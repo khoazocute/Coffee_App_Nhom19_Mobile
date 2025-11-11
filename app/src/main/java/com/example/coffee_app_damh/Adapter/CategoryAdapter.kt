909c1217676b11b92d3ea5897b5356ea52514ea1
khoazocute <vodangkhoa0524@gmail.com>
2025-11-11 18:38:39 +0700

    backend category

---
+++ app/src/main/java/com/example/coffee_app_damh/Adapter/CategoryAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.coffee_app_damh.Adapter
+import com.example.coffee_app_damh.R
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coffee_app_damh.Domain.CategoryModel
+import com.example.coffee_app_damh.databinding.ActivityMainBinding
+import com.example.coffee_app_damh.databinding.ViewholderCategoryBinding
+
+
+class CategoryAdapter(val items: MutableList<CategoryModel>):
+    RecyclerView.Adapter<CategoryAdapter.Viewholder>(){
+        private lateinit var context: Context
+        private var selectedPosition=-1
+    private var lastSelectedPosition=-1
+    inner class Viewholder(var binding: ViewholderCategoryBinding)
+        : RecyclerView.ViewHolder(binding.root)
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CategoryAdapter.Viewholder {
+        context=parent.context
+        val binding= ViewholderCategoryBinding.inflate(LayoutInflater.from(context),parent,false)
+        return Viewholder(binding)// Tạo ra 1 ViewHolder mới cho từng item
+    }
+
+    override fun onBindViewHolder(holder: CategoryAdapter.Viewholder, position: Int) {
+        val item=items[position] //Lấy category tại vị trí hiện tại
+        holder.binding.titleCat.text=item.title
+
+        holder.binding.root.setOnClickListener {
+            lastSelectedPosition=selectedPosition
+            selectedPosition=position
+            notifyItemChanged(lastSelectedPosition)
+            notifyItemChanged(selectedPosition)
+        }
+        if(selectedPosition==position){
+            holder.binding.titleCat.setBackgroundResource(R.drawable.dark_brown_bg) // Đổi nền sang nâu đậm
+            holder.binding.titleCat.setTextColor(context.resources.getColor(R.color.white)) // Chữ thành trắng
+        } else {
+            holder.binding.titleCat.setBackgroundResource(R.drawable.white_bg) // Đổi nền sang nâu đậm
+            holder.binding.titleCat.setTextColor(context.resources.getColor(R.color.darkBrown)) // Chữ thành trắng
+        }
+
+    }
+
+    override fun getItemCount(): Int = items.size
+
+}
---
+++ app/src/main/java/com/example/coffee_app_damh/ViewModel/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.coffee_app_damh.ViewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.coffee_app_damh.Domain.BannerModel
+import com.example.coffee_app_damh.Domain.CategoryModel
+import com.example.coffee_app_damh.Repository.MainRepository
+
+class MainViewModel: ViewModel() {
+    private val repository = MainRepository()
+
+    fun loadBanner(): LiveData<MutableList<BannerModel>>{
+        return repository.loadBanner()
+    }
+
+    fun loadCategory(): LiveData<MutableList<CategoryModel>>{
+        return repository.loadCategory()
+    }
+}
