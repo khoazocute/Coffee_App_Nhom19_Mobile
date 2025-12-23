31a709cbcc387711e9da8848767ded0382e1fcf3
d23520229 <dangnguyen122205@gmail.com>
2025-12-23 16:18:32 +0700

    Giao diện cho quản lý khuyến mãi của admin

--- app/src/main/java/com/example/coffee_app_damh/Domain/PromotionModel.kt
+++ app/src/main/java/com/example/coffee_app_damh/Domain/PromotionModel.kt
@@ -3,7 +3,10 @@ package com.example.coffee_app_damh.Domain
 import java.io.Serializable
 
 data class PromotionModel(
-    var code: String = "",
-    var discountPercent: Int = 0, // Ví dụ: 10, 20, 50
-    var description: String = ""
+    var id: String = "",           // Dùng code làm ID luôn cho dễ quản lý
+    var code: String = "",         // Mã hiển thị (VD: SALE50)
+    var discountPercent: Int = 0,  // Số % giảm
+    var description: String = "",  // Mô tả chương trình
+    var startDate: Long = 0,       // Ngày bắt đầu (Timestamp)
+    var endDate: Long = 0          // Ngày kết thúc (Timestamp)
 ) : Serializable
